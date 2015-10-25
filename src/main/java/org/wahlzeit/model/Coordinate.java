a3a20e23e5f4b79d85983675b5127f6738684144
Nadja Färber <nadjafaerber3012@gmail.com>
2015-10-25 20:14:27 +0100

    class "Coordinate" created

---
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package org.wahlzeit.model;
+
+/**
+ * @author faerbena
+ *
+ */
+class Coordinate {
+	private double latitude;
+	private double longitude;
+
+	/**
+	 * @methodtype constructor
+	 */
+	public Coordinate(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public double getLatitude() {
+		return this.latitude;
+	}
+
+	/**
+	 * @methodtype get
+	 */	
+	public double getLongitude() {
+		return this.longitude;
+	}
+	
+	/**
+	 * @methodtype set
+	 */	
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	
+	/**
+	 * @methodtype set
+	 */	
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+	/**
+	 * @methodtype query
+	 */	
+	public Coordinate getDistance(Coordinate c) {
+		double latitudinalDistance = getLatitudinalDistance(c);
+		double longitudinalDistance = getLongitudinalDistance(c);
+		Coordinate distance = new Coordinate(latitudinalDistance, longitudinalDistance);
+		return distance;
+	}
+	
+	/**
+	 * @methodtype query
+	 */	
+	public double getLatitudinalDistance(Coordinate c) {
+		double latitudinalDistance = this.getLatitude() - c.getLatitude();
+		return latitudinalDistance;
+	}
+	
+	/**
+	 * @methodtype query
+	 */	
+	public double getLongitudinalDistance(Coordinate c) {
+		double longitudinalDistance = this.getLongitude() - c.getLongitude();
+		return longitudinalDistance;
+	}
+
+}
